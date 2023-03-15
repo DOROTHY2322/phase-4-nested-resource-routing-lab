8b1d0e8183f2029b0a5e86c3c3127f2defd889b8
DOROTHY2322 <kimelidorothy@gmail.com>
2023-03-15 21:38:12 +0300

    nested resource lab

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,37 @@
 class ItemsController < ApplicationController
+  before_action :find_user
 
   def index
-    items = Item.all
+    items = @user.items
     render json: items, include: :user
   end
 
+  def show
+    item = @user.items.find_by(id: params[:id])
+    if item
+      render json: item, include: :user
+    else
+      render json: { error: "Item not found" }, status: :not_found
+    end
+  end
+
+  def create
+    item = @user.items.new(item_params)
+    if item.save
+      render json: item, status: :created
+    else
+      render json: { error: item.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+  end
+
+  def find_user
+    @user = User.find_by(id: params[:user_id])
+    render json: { error: "User not found" }, status: :not_found unless @user
+  end
 end
